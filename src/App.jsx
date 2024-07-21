9570dc90598b298233d7f8b747bbc409540cf129
Nikhilkumhar <nikhilkumhar57@gmail.com>
2024-07-21 18:16:08 +0530

    carousel

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import CarouselPage from './CarouselPage';
+import Navigationbar from './Navigationbar'
+
+
+
+
+function App() {
+  return (
+    
+    <div className="App">
+       <Navigationbar/>
+       <CarouselPage/>
+           
+    </div>
+    
+  );
+}
+
+export default App;
